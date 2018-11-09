c5ccf439e33389decf9a7b3b600d83d41ecb44a1
nekobato <nekobato@gmail.com>
2018-11-09 19:54:24 +0900

    nanikore

---
+++ src/sitedata.js
@@ -0,0 +1,10 @@
+module.exports = {
+  profile: {
+    name: "nekobato",
+    realname: "Hayato Koriyama",
+    email: "nekobato@gmail.com",
+    tel: "090.7301.4492",
+    place: "Asia/Tokyo",
+    links: {}
+  }
+};
