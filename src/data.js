13d72149b17e89c77d0d5dea03c3e84903ee0d03
Hayato Koriyama <nekobato@gmail.com>
2019-03-18 23:53:05 +0900

    2019 (#4)
    * nanikore
    
    * hoge
    
    * chotto style
    
    * hoge
    
    * add 404
    
    * init for firebase
    
    * dev init

---
+++ src/data.js
@@ -0,0 +1,91 @@
+module.exports = {
+  profile: {
+    handle: '猫鳩',
+    realname: 'Hayato Koriyama',
+    timezone: 'Asia/Tokyo',
+    links: [{
+        title: 'Github',
+        icon: 'fab fa-github',
+        url: 'https://github.com/nekobato'
+      },
+      {
+        title: 'Twitter',
+        icon: 'fab fa-twitter',
+        url: 'https://twitter.com/nekobato'
+      },
+      {
+        title: 'Email',
+        icon: 'fas fa-envelope',
+        url: 'mailto:nekobato@gmail.com'
+      },
+      {
+        title: 'Tel',
+        icon: 'fas fa-phone',
+        url: 'tel:09073014492'
+      }
+    ]
+  },
+  education: [{
+      name: '慶応義塾大学 環境情報学部',
+      from: '2009/04',
+      to: '2013/03'
+    },
+    {
+      name: '慶應義塾大学院 政策・メディア学科 CIプログラム 修士課程',
+      from: '2013/04',
+      to: '2015/03'
+    }
+  ],
+  experience: [{
+      name: '株式会社 U-NOTE - 業務委託エンジニア',
+      from: '2012/04',
+      to: '2015/03'
+    },
+    {
+      name: '株式会社 U-NOTE - 業務委託エンジニア',
+      from: '2017/01',
+      to: 'now'
+    },
+    {
+      name: '株式会社 モバイルファクトリー - フロントエンドエンジニア',
+      from: '2015/04',
+      to: '2018/06'
+    },
+    {
+      name: '株式会社 メルカリ - フロントエンドエンジニア',
+      from: '2018/07',
+      to: 'now'
+    }
+  ],
+  resume: {},
+  apps: [{
+      name: 'Polidium',
+      text: '作業と動画視聴を両立するためのメディアプレイヤー',
+      url: 'http://github.com/nekobato/Polidium'
+    },
+    {
+      name: 'Davneko',
+      text: 'Web音楽プレイヤー',
+      url: 'http://github.com/nekobato/Davneko'
+    },
+    {
+      name: 'BookCafe',
+      text: 'Webブックリーダー',
+      url: 'http://github.com/nekobato/BookCafe'
+    },
+    {
+      name: 'ScrapLinker',
+      text: '開いているタブのリンクをScrapbox記法で生成するChrome extension',
+      url: 'http://github.com/nekobato/ScrapLinker'
+    },
+    {
+      name: 'Endnanonna',
+      text: 'GIFジェネレーター',
+      url: 'http://github.com/nekobato/endnanonna'
+    }
+  ],
+  links: [{
+    title: '',
+    url: ''
+  }]
+};
