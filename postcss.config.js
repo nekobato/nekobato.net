2c982c667af115c7a31324f7f52ddb3cf28e4ba6
nekobato <nekobato@gmail.com>
2022-07-01 02:00:39 +0900

    refactor: migrate percel to vite

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-{
-  "plugins": {
+module.exports = {
+  plugins: {
     "postcss-import": true,
     "postcss-nested": true
   }
-}
+};
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import "./style.pcss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const main = document.querySelector("#main") as HTMLDivElement;
+  const dummyContent = document.querySelector(
+    "#dummy-content"
+  ) as HTMLDivElement;
+
+  const setDummyContentHeight = () => {
+    if (!dummyContent || !main) return;
+    dummyContent.style.height =
+      window.innerHeight * (main.scrollHeight / main.clientHeight) + "px";
+  };
+
+  const onScrollWindow = () => {
+    main.scrollTop = (window.scrollY / window.innerHeight) * main.clientHeight;
+  };
+
+  window.addEventListener("scroll", onScrollWindow, { passive: true });
+  window.addEventListener("resize", setDummyContentHeight, { passive: true });
+  main.addEventListener("scroll", e => {
+    e.stopPropagation();
+  });
+
+  setDummyContentHeight();
+  document.querySelector('body')?.classList.remove('loading');
+});
